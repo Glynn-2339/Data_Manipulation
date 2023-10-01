c63b44ec2906c79d3d2074cb64aaf5a8bddb713b
Glynn-2339 <137445160+Glynn-2339@users.noreply.github.com>
2023-10-01 18:24:21 +0300

    en.py

--- en.py
+++ en.py
@@ -26,4 +26,24 @@ df.to_excel(xlsx_file_path, index=False)
 
 print(f"Excel file has been generated and saved to {xlsx_file_path}.")
 
+"""
+import zipfile
+import os
+
+zip_file_path = "D:/CodingProjects/PythonProjects/amazon-massive-dataset-1.1_CAT1.tar.zip"
+output_folder = "D:/CodingProjects/PythonProjects/output_folder"  # Specify the output folder
+
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+# Extract en-US.jsonl from the Zip archive to the output folder
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extract("1.1/data/en-US.jsonl", os.path.join(output_folder, "en-US.jsonl"))
+
+print(f"File en-US.jsonl has been extracted to {output_folder}.")
+"""
+"""
+The commented code abpve is code number 2 I tried
+"""
+
 
