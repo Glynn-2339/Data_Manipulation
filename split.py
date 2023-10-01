bdcc19bea78c76415f6bdc5d29b8ba9a99d5303b
Glynn-2339 <137445160+Glynn-2339@users.noreply.github.com>
2023-10-01 17:58:05 +0300

    split.py

---
+++ split.py
@@ -0,0 +1,43 @@
+"""
+This Code I used it to enerate separate jsonl files with test, train and dev respectively. 
+"""
+import os
+import pandas as pd
+import json
+
+# Directory where all the JSON Lines files are located
+jsonl_files_dir = "D:/CodingProjects/PythonProjects/1.1/data"
+
+# Directory where you want the Excel files for the languages to be stored
+output_folder = "D:/CodingProjects/PythonProjects/languages_output"
+
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+# Get a list of all JSON Lines files in the directory
+jsonl_files = [f for f in os.listdir(jsonl_files_dir) if f.endswith(".jsonl")]
+
+# Iterate through each JSON Lines file
+for jsonl_file in jsonl_files:
+    # Create a full path to the JSON Lines file
+    jsonl_file_path = os.path.join(jsonl_files_dir, jsonl_file)
+
+    # Extract language code from file name
+    language_code = os.path.splitext(jsonl_file)[0]
+
+    # Load the JSON Lines data into a DataFrame
+    data = []
+    with open(jsonl_file_path, "r", encoding="utf-8") as jsonl_file:
+        for line in jsonl_file:
+            data.append(json.loads(line))
+
+    # Create a DataFrame from the loaded data
+    df = pd.DataFrame(data)
+
+    # Specify the path to save the Excel file for the language in the output folder
+    xlsx_file_path = os.path.join(output_folder, f"{language_code}.xlsx")
+
+    # Save the DataFrame as an Excel file in the output folder
+    df.to_excel(xlsx_file_path, index=False)
+
+    print(f"Excel file for {language_code} has been generated and saved to {xlsx_file_path}.")
